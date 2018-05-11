eeae74db82e13cf2446029951aae9b2490ede06a
Asep Maulana Ismail <asepmaulanaismail@gmail.com>
2018-05-11 10:27:16 +0700

    add clients migration and seeder

---
+++ database/migrations/2018_05_11_032131_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bpm_clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email', 25);
+            $table->string('password', 255);
+            $table->string('name', 25);
+            $table->integer("parent_id");
+            $table->boolean('isActive')->default(true);
+            $table->integer("created_by")->default(1);
+            $table->dateTime('created_dt')->default(date('Y-m-d H:i:s'));
+            $table->integer("updated_by")->default(1);
+            $table->dateTime('updated_dt')->default(date('Y-m-d H:i:s'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bpm_clients');
+    }
+}
---
+++ database/seeds/ClientsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClientsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('bpm_clients')->insert([
+            'id' => 1,
+            'email' => 'client@bpm.com',
+            'password' => sha1('bsp123!!'),
+            'name' => "Client",
+            'parent_id' => 1
+        ]);
+    }
+}
